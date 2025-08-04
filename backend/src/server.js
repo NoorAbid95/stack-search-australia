3509821fc3d97bcd7173467c54dd669e0ee581a0
Noor Abid <your.email@example.com>
2025-08-04 13:13:36 +1000

    Prepared application for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,22 +2,33 @@ import express from "express";
 import dotenv from "dotenv";
 import { jobSearch } from "./controllers/jobSearch.controller.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
 
 app.use("/api/jobs", jobSearch);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("App running on localhost:", PORT);
